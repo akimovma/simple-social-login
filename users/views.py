b4f195ac7a672d6c2379bd94e86faa273acc97bb
Michael Akimov <akimov.michail@gmail.com>
2017-10-12 22:55:34 +0300

    initial2

--- users/views.py
+++ users/views.py
@@ -1,10 +1,5 @@
-from django.urls import reverse
 from django.views.generic import TemplateView
-from allauth.account.views import LoginView
 
 
 class HomeView(TemplateView):
     template_name = 'home.html'
-
-    def get_success_url(self):
-        return reverse('home')
